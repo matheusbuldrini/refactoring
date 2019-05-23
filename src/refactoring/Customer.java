52afa8634ad31713b47ea0a4d566efb3f0bdc28b
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-05-23 20:57:10 -0300

    Quinto COMMIT & PUSH

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -18,27 +18,6 @@ class Customer {
       return _name;
    }
   
- private double amountFor(Rental aRental) {
-   //determine amounts for each line
-   double thisAmount = 0;
-   switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-         thisAmount += 2;
-         if (aRental.getDaysRented() > 2)
-            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-         break;
-      case Movie.NEW_RELEASE:
-         thisAmount += aRental.getDaysRented() * 3;
-         break;
-      case Movie.CHILDRENS:
-         thisAmount += 1.5;
-         if (aRental.getDaysRented() > 3)
-            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-          break;
-   }
-   return thisAmount;
- }
-   
  public String statement() {
 	   double totalAmount = 0;
 	   int frequentRenterPoints = 0;
@@ -48,7 +27,7 @@ class Customer {
 	      double thisAmount = 0;
 	      Rental each = (Rental) rentals.nextElement();
 
-	      thisAmount = amountFor(each);
+	      thisAmount = each.getCharge();
 
 	      // add frequent renter points
 	      frequentRenterPoints ++;
