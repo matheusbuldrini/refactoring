815ad7ae1030de26da5d2794d144608e4b51aed5
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-05-23 20:53:56 -0300

    Quarto COMMIT & PUSH

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -18,22 +18,22 @@ class Customer {
       return _name;
    }
   
- private double amountFor(Rental rental) {
+ private double amountFor(Rental aRental) {
    //determine amounts for each line
    double thisAmount = 0;
-   switch (rental.getMovie().getPriceCode()) {
+   switch (aRental.getMovie().getPriceCode()) {
       case Movie.REGULAR:
          thisAmount += 2;
-         if (rental.getDaysRented() > 2)
-            thisAmount += (rental.getDaysRented() - 2) * 1.5;
+         if (aRental.getDaysRented() > 2)
+            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
          break;
       case Movie.NEW_RELEASE:
-         thisAmount += rental.getDaysRented() * 3;
+         thisAmount += aRental.getDaysRented() * 3;
          break;
       case Movie.CHILDRENS:
          thisAmount += 1.5;
-         if (rental.getDaysRented() > 3)
-            thisAmount += (rental.getDaysRented() - 3) * 1.5;
+         if (aRental.getDaysRented() > 3)
+            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
           break;
    }
    return thisAmount;
