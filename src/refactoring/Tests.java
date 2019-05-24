be5742bfcbef93114c0122b7b87ea990978f0587
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2019-05-23 21:17:37 -0300

    Nono COMMIT & PUSH

--- src/refactoring/Tests.java
+++ src/refactoring/Tests.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 public class Tests {
       
 	@Test
-	public void testMultiplication() {
+	public void testStatement() {
 		   Movie vingadores = new Movie("Vingadores Ultimato", Movie.NEW_RELEASE);
 		   Movie chiquititas = new Movie("Chiquititas", Movie.CHILDRENS);
 		   
@@ -24,6 +24,25 @@ public class Tests {
 		   		"	Chiquititas	7.5\n" + 
 		   		"Amount owed is 16.5\n" + 
 		   		"You earned 3 frequent renter points", cliente.statement());
+	}
+	
+	@Test
+	public void testHtmlStatement() {
+		   Movie vingadores = new Movie("Vingadores Ultimato", Movie.NEW_RELEASE);
+		   Movie chiquititas = new Movie("Chiquititas", Movie.CHILDRENS);
+		   
+		   Rental aluguel1 = new Rental(vingadores, 3);
+		   Rental aluguel2 = new Rental(chiquititas, 7);
+		   
+		   Customer cliente = new Customer("Manolo");
+		   cliente.addRental(aluguel1);
+		   cliente.addRental(aluguel2);
+		   
+		   assertEquals("<H1>Rentals for <EM>Manolo</EM></H1><P>\n" + 
+		   		"Vingadores Ultimato: 9.0<BR>\n" + 
+		   		"Chiquititas: 7.5<BR>\n" + 
+		   		"<P>You owe <EM>16.5</EM><P>\n" + 
+		   		"On this rental you earned <EM>3</EM> frequent renter points<P>", cliente.htmlStatement());
 	}
 
 }
